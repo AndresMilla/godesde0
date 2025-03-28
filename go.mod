83ac703d5f5efad6a7fcd2f2b8a4b35cf1dfcf13
AndresMilla <andresmilla11@gmail.com>
2025-03-27 20:00:13 -0600

    Primer Comit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AndresMilla/godesde0
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/AndresMilla/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
