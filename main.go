3aae48eed42a9bbd12b90977e2af69633715d689
AndresMilla <andresmilla11@gmail.com>
2025-03-30 22:24:10 -0600

    GoRoutines

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"github.com/AndresMilla/godesde0/ejer_interfaces"
-	"github.com/AndresMilla/godesde0/modelos"
+	"fmt"
+
+	"github.com/AndresMilla/godesde0/goroutines"
 )
 
 func main() {
 	/* estado, texto := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
-	fmt.Println(texto)
-	if os := runtime.GOOS; os == "linux" || os == "OS X" {
-		fmt.Println("Esto no es windows ", os)
-	} else {
-		fmt.Println("Esto es:", os)
-	}
+	   fmt.Println(estado)
+	   fmt.Println(texto)
+	   if os := runtime.GOOS; os == "linux" || os == "OS X" {
+	   	fmt.Println("Esto no es windows ", os)
+	   } else {
+	   	fmt.Println("Esto es:", os)
+	   }
 
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es linux")
-	case "darwin":
-		fmt.Println("Esto es darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
+	   switch os := runtime.GOOS; os {
+	   case "linux":
+	   	fmt.Println("Esto es linux")
+	   case "darwin":
+	   	fmt.Println("Esto es darwin")
+	   default:
+	   	fmt.Printf("%s \n", os)
+	   }
 
-	numero, texto := ejercicios.ConvNumerico("500")
-	fmt.Println(numero)
-	fmt.Println(texto)*/
+	   numero, texto := ejercicios.ConvNumerico("500")
+	   fmt.Println(numero)
+	   fmt.Println(texto)*/
 
 	//teclado.IngresoNumeros()
 
@@ -46,9 +47,17 @@ func main() {
 
 	//mapas.MostrarMapas()
 
-	Pedro := new(modelos.Hombre)
-	ejer_interfaces.HumanosRespirando(Pedro)
+	/*Pedro := new(modelos.Hombre)
+	  ejer_interfaces.HumanosRespirando(Pedro)
 
-	Maria := new(modelos.Mujer)
-	ejer_interfaces.HumanosRespirando(Maria)
+	  Maria := new(modelos.Mujer)
+	  ejer_interfaces.HumanosRespirando(Maria)*/
+
+	//defer_panic.EjemploPanic()
+
+	go goroutines.MiNombreLentooo("Sergio Andres Milla Cano")
+
+	fmt.Println("Estoy aqui")
+	var x string
+	fmt.Scanln(&x)
 }
